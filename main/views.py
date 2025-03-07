b80e984afb370c97dfd448d890d5401eb653d2d5
Yaroslav B. <debian1500@gmail.com>
2025-03-07 13:43:52 +0500

    Version 0.1.1

--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def curr_datetime(request):
+    now = datetime.now()
+    context = {
+            'curr_date': now.strftime("%d.%m.%y"),
+            'curr_time': now.strftime("%H:%M:%S")
+        }
+    return render(request, 'main/datetime.html', context)  
+
