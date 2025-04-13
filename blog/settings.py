912782b1714160c62c2f06645928812b3506524d
Yaroslav <debian1500@gmail.com>
2025-04-13 14:18:30 +0500

    Version 0.2

--- blog/settings.py
+++ blog/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'users',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -127,3 +132,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from django.urls import path
-from main import views
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-   
+from users import views as users_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about'),
-    
-] + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+    path('register/', users_views.register, name='register'),
+    path('profile/', users_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('', include('main.urls')),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
